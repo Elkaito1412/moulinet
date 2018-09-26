9b2dfc046d6cbe5a828cddbfced8f8dd6cd9c7c3
KernelOverseer <aymanebiri@gmail.com>
2018-09-26 11:50:49 +0100

    doing good

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 #this ia a try at making a mini local version of the moulinette
 import os
+from os import walk
 import sys
 from tcolors import *
 from usage import *
 
-print tcolors.HEADER + "-----Moulinet v01-----\n" + tcolors.ENDC
+bindir = os.path.realpath(__file__)
+version = "01"
+
+try:
+	configfile = open(".moulinet.conf", "r")
+except IOError:
+	print(tcolors.WARNING + "WARNNG : directory not initialised" + tcolors.ENDC)
+print tcolors.HEADER + "-----Moulinet v" + version + "-----\n" + tcolors.ENDC
 if len(sys.argv) != 2 :
 	usage();
 	exit();
+elif sys.argv[1] == "init":
+	print "initialising moulinet in folder : " + tcolors.OKBLUE + os.path.abspath(__file__) + '\n'
+	os.system("cp ~/.moulinet/moulinet.default.conf ./.moulinet.conf")
+elif sys.argv[1] == "purge":
+	print "purging all moulinet files in folder : " + tcolors.OKBLUE + os.path.abspath(__file__) + '\n'
+	os.system("rm -f .moulinet.conf")
+elif sys.argv[1] == "list":
+	filenum = 0;
+	for (dirpath, dirnames, filenames) in walk("./projects"):
+    		for i in filenames:
+			print ' ' + tcolors.OKBLUE + str(filenum) + ' : ' + tcolors.ENDC + i
+			filenum = filenum + 1
+		print '\n'
+    		break
+elif sys.argv[1] == "grademe":
+	try:
+		configfile
+	except NameError:
+		print( tcolors.BOLD + tcolors.FAIL + "please initialise directory and chose project before grading !\n")
+		exit()
+	if "proj_id = null" in configfile.read():
+		print(tcolors.BOLD + tcolors.FAIL + "project not chosen \n")
+	else:
+		os.system("echo project found")
+else:
+	plugins = []
+	for (dirpath, dirnames, filenames) in walk("./projects"):
+		plugins.extend(filenames);
+	if(int(sys.argv[1]) < len(filenames)):
+		print("loading project : " + filenames[int(sys.argv[1])])
+		os.system("cat .moulinet.conf | sed 's/proj_id = */proj_id = " + sys.argv[1] + "'/g > .moulinet.conf")
+	else:
+		print(" project not available at the moment\n")
--- usage.py
+++ usage.py
@@ -1,8 +1,9 @@
 from tcolors import *
 
 def	usage():
-	print """Usage :
-""" + tcolors.OKGREEN + """init : """ + tcolors.ENDC + """initialise he moulinet in the folder
-""" + tcolors.OKGREEN + """purge : """ + tcolors.ENDC + """purge all moulinet files in current directory
-""" + tcolors.OKGREEN + """grademe : """ + tcolors.ENDC + """evaluate user files in the directory and write traces
-""" + tcolors.OKGREEN + """<exercise id> : """ + tcolors.ENDC + """use this directory as containing solution to exercise with the specified id\n"""
+	print tcolors.BOLD + """Usage :
+""" + tcolors.OKGREEN + """init : """ + tcolors.ENDC + """initialise he moulinet in the folder.
+""" + tcolors.BOLD + tcolors.OKGREEN + """purge : """ + tcolors.ENDC + """purge all moulinet files in current directory.
+""" + tcolors.BOLD + tcolors.OKGREEN + """grademe : """ + tcolors.ENDC + """evaluate user files in the directory and write traces.
+""" + tcolors.BOLD + tcolors.OKGREEN + """list :""" + tcolors.ENDC + """ list all supported projects. 
+""" + tcolors.BOLD + tcolors.OKGREEN + """<exercise id> : """ + tcolors.ENDC + """use this directory as containing solution to exercise with the specified id.\n"""
